c66cec1f6bde9619dfc457eb838f5cfbc9ba5459
Rose <rose.koron@hashicorp.com>
2024-09-12 11:51:06 -0700

    defaults

--- variables.tf
+++ variables.tf
@@ -1,9 +1,11 @@
 variable "strings" {
   type        = number
   description = "Number of strings to generate"
+  default = 1
 }
 
 variable "length" {
   type        = number
   description = "Length of each random string"
+  default = 5
 }
